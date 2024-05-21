489fc288ed4d46b5bf94d483bc9626f9512ae5a8
FCostaM <felipe0211@hotmail.com>
2024-05-21 00:35:33 -0300

    feat(CashFlow.Api): adiciona controller de despesa

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Application.UseCases.Expenses;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] ExpenseRegisterRequest request)
+    {
+        var response = new ExpenseRegisterUseCase().Execute(request);
+        
+        return Created(string.Empty, response);
+    }
+}
