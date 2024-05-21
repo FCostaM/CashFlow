a5484eeae28974f429dc51550d146dd6ee4fa69c
FCostaM <felipe0211@hotmail.com>
2024-05-21 00:16:31 -0300

    feat(CashFlow.Application): adiciona classe de caso de uso para registro de despesa

---
+++ src/CashFlow.Application/UseCases/Expenses/ExpenseRegisterUseCase.cs
@@ -0,0 +1,12 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses;
+
+public class ExpenseRegisterUseCase
+{
+    public ExpenseResponse Execute(ExpenseRegisterRequest request)
+    {
+        return new ExpenseResponse();
+    }
+}
