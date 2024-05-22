1c03a008d2ab4a7acc19c901d0e92041048919ba
FCostaM <felipe0211@hotmail.com>
2024-05-21 21:59:06 -0300

    feat(CashFlow.Communication): adiciona classe de retorno de erro

---
+++ src/CashFlow.Communication/Responses/ErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlow.Communication.Responses;
+
+public class ErrorResponse
+{
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    public ErrorResponse(string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+    }
+}
