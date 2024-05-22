eab56873a8dd71077e9330f4c792971a34ec9a6e
FCostaM <felipe0211@hotmail.com>
2024-05-21 23:37:56 -0300

    feat(CashFlow.Application): adiciona classe de validação da request usando FluentValidation

---
+++ src/CashFlow.Application/Validators/ExpenseRegisterValidator.cs
@@ -0,0 +1,23 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.Validators;
+
+public class ExpenseRegisterValidator : AbstractValidator<ExpenseRegisterRequest>
+{
+    public ExpenseRegisterValidator()
+    {
+        RuleFor(expense => expense.Title)
+            .NotEmpty()
+            .WithMessage("Title is required");
+        RuleFor(expense => expense.Date)
+            .LessThanOrEqualTo(DateTime.UtcNow)
+            .WithMessage("Expense cannot be for the future");
+        RuleFor(expense => expense.Amount)
+            .GreaterThan(0)
+            .WithMessage("Amount must be greater then zero");
+        RuleFor(expense => expense.PaymentType)
+            .IsInEnum()
+            .WithMessage("Payment type is invalid");
+    }
+}
