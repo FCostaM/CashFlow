f313ff66f2844cc28c2a85205ea3add8cb0fd97e
FCostaM <felipe0211@hotmail.com>
2024-05-26 00:31:45 -0300

    feat(UnitTests): adiciona os testes de falha na validação dos campos da request de registro de despesa

--- tests/Validators.Tests/Expenses/ExpenseRegisterValidatorTests.cs
+++ tests/Validators.Tests/Expenses/ExpenseRegisterValidatorTests.cs
@@ -1,5 +1,8 @@
 ﻿using CashFlow.Application.Validators;
+using CashFlow.Communication.Enums;
+using CashFlow.Exception.Resources;
 using CommonTestUtilities.Requests;
+using FluentAssertions;
 
 namespace Validators.Tests.Expenses;
 
@@ -16,6 +19,92 @@ public class ExpenseRegisterValidatorTests
         var result = validator.Validate(request);
 
         //Assert
-        Assert.True(result.IsValid);
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("         ")]
+    [InlineData(null)]
+    public void TitleEmptyFailure(string title)
+    {
+        //Arrange
+        var validator = new ExpenseRegisterValidator();
+        var request = ExpenseRegisterRequestBuilder.Build();
+        request.Title = title;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+
+        result.Errors
+            .Should()
+            .ContainSingle().And
+            .Contain(e => e.ErrorMessage.Equals(ErrorMessageResource.TITLE_REQUIRED));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-10)]
+    public void AmountInvalidFailure(decimal amount)
+    {
+        //Arrange
+        var validator = new ExpenseRegisterValidator();
+        var request = ExpenseRegisterRequestBuilder.Build();
+        request.Amount = amount;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+
+        result.Errors
+            .Should()
+            .ContainSingle().And
+            .Contain(e => e.ErrorMessage.Equals(ErrorMessageResource.AMOUNT_UNDER_ZERO));
+    }
+
+    [Fact]
+    public void FutureDateFailure()
+    {
+        //Arrange
+        var validator = new ExpenseRegisterValidator();
+        var request = ExpenseRegisterRequestBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+
+        result.Errors
+            .Should()
+            .ContainSingle().And
+            .Contain(e => e.ErrorMessage.Equals(ErrorMessageResource.PAST_DATE_EXPENSE));
+    }
+
+    [Fact]
+    public void PaymentTypeInvalidFailure()
+    {
+        //Arrange
+        var validator = new ExpenseRegisterValidator();
+        var request = ExpenseRegisterRequestBuilder.Build();
+        request.PaymentType = (PaymentType)700;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+
+        result.Errors
+            .Should()
+            .ContainSingle().And
+            .Contain(e => e.ErrorMessage.Equals(ErrorMessageResource.PAYMENT_TYPE_INVALID));
     }
 }
