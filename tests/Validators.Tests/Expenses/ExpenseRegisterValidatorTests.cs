51f0ad0616ac9ea9ed8f5ae18694abb9cf3d0e83
FCostaM <felipe0211@hotmail.com>
2024-05-25 23:15:54 -0300

    feat(CashFlow.tests): adiciona teste unitário para o validador da request de registro de despesa

---
+++ tests/Validators.Tests/Expenses/ExpenseRegisterValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow.Application.Validators;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses;
+
+public class ExpenseRegisterValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new ExpenseRegisterValidator();
+        var request = new ExpenseRegisterRequest()
+        {
+            Title = "Test",
+            Description = "Test",
+            Amount = 1,
+            Date = DateTime.Now.AddDays(-1),
+            PaymentType = CashFlow.Communication.Enums.PaymentType.Cash
+        };
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}
