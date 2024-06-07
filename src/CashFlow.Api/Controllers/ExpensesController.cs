31ec22aa758d1f11f349da5ce1698707df5a2a66
FCostaM <felipe0211@hotmail.com>
2024-06-07 00:09:00 -0300

    feat(CashFlow.Api): adiciona documentação dos tipos de retorno produzidos pelo endpoint de registro de despesa

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,11 +1,14 @@
 ﻿using CashFlow.Application.Interfaces;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[ProducesResponseType(typeof(ExpenseResponse), StatusCodes.Status201Created)]
+[ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
 public class ExpensesController : ControllerBase
 {
     [HttpPost]
