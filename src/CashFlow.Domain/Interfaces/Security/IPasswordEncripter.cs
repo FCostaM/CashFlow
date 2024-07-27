0312a72d852e9225ed722a8a3e8cf9730dec448f
FCostaM <felipe0211@hotmail.com>
2024-07-27 00:37:23 -0300

    feat: Adiciona método de comparação entre a senha criptograda no banco de dados e senha informada no login.

--- src/CashFlow.Domain/Interfaces/Security/IPasswordEncripter.cs
+++ src/CashFlow.Domain/Interfaces/Security/IPasswordEncripter.cs
@@ -11,4 +11,12 @@ public interface IPasswordEncripter
     /// <param name="password">The plain text password to encrypt.</param>
     /// <returns>The encrypted password.</returns>
     string Encrypy(string password);
+
+    /// <summary>
+    /// Verifies if the provided password matches the hashed password.
+    /// </summary>
+    /// <param name="password">The plain text password to verify.</param>
+    /// <param name="hash">The hashed password to compare against.</param>
+    /// <returns>True if the password matches the hash; otherwise, false.</returns>
+    bool DoesPasswordMatch(string password, string hash);
 }
--- src/CashFlow.Infrastructure/Security/PasswordEncripter.cs
+++ src/CashFlow.Infrastructure/Security/PasswordEncripter.cs
@@ -17,4 +17,15 @@ internal class PasswordEncripter : IPasswordEncripter
     {
         return BC.HashPassword(password);
     }
+
+    /// <summary>
+    /// Verifies if the provided password matches the hashed password.
+    /// </summary>
+    /// <param name="password">The plain text password to verify.</param>
+    /// <param name="hash">The hashed password to compare against.</param>
+    /// <returns>True if the password matches the hash; otherwise, false.</returns>
+    public bool DoesPasswordMatch(string password, string hash)
+    {
+        return BC.Verify(password, hash);
+    }
 }
